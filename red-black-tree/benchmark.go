4f143964abd088b06d6d930699ebc7d27089a3de
4llower <95350098@study.bsuir.by>
2021-05-20 10:18:53 +0300

    add: tests

--- red-black-tree/benchmark.go
+++ red-black-tree/benchmark.go
@@ -1,6 +1,10 @@
 package main
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 import "container/list"
 import searchTree "./search_tree"
 
@@ -10,26 +14,39 @@ type ArrayValue struct {
 	Value int
 }
 
-func main() {
+func hardTest() bool {
+	println("--- HARD TEST START ---")
 	tree := searchTree.CreateSearchTree([]int{})
 	sourceArray := list.New()
+
+	start := time.Now()
 
 	for i := 0; i < MAXN; i++ {
 		newValue := rand.Int()
 		sourceArray.PushBack(newValue)
 		tree.Insert(newValue)
-		//println(newValue)
 	}
 
-	//println("---- end ----")
-
 	for it := sourceArray.Front(); it != nil; it = it.Next() {
-		//println((*it).Value.(int))
 		if tree.Find((*it).Value.(int)) == nil {
-			print("NOT PASSED!")
-			return
+			println("NOT PASSED!")
+			return false
 		}
 	}
 
-	print("PASSED")
+	println(fmt.Sprintf("PASSED at %f with %d elements", time.Now().Sub(start).Seconds(), MAXN))
+	println("--- HARD TEST END ---\n")
+	return true
+}
+
+func main() {
+	passedTests := 0
+	allTests := 1
+	println("START TESTING...\n")
+
+	if hardTest() {
+		passedTests++
+	}
+
+	println(fmt.Sprintf("--- PASSED %d / %d ---", passedTests, allTests))
 }
